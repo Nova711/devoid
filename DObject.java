66521bd9c140bcfffd4c5cb89aa070dfd0fe269e
Nova711 <39838626+Nova711@users.noreply.github.com>
2019-12-02 13:17:22 -0500

    Fixed

---
+++ DObject.java
@@ -0,0 +1,45 @@
+package devoid_boosted;
+
+import java.awt.Graphics;
+
+public interface DObject {
+
+	double getHP();
+
+	double getX();
+
+	double getY();
+	
+	double getAngle();
+	
+	double getMass();
+	
+	double getI();
+
+	void setX(double x);
+
+	void setY(double y);
+	
+	void setAngle(double angle);
+
+	Vector getVelocity();
+
+	void setVelocity(Vector velocity);
+
+	double getAngularVelocity();
+
+	void setAngularVelocity(double velocity);
+	
+	double getTemperature();
+	
+	void setTemperature(double temperature);
+
+	void update();
+
+	void impact(DObject obj);
+
+	boolean hits(DObject[] objects);
+	
+	void draw(Graphics g);
+
+}
